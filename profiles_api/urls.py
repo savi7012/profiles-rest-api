f848499d9df6e277d85fa702d1d4b6d68b6e7da0
savi7012 <vishalsaini72@gmail.com>
2020-02-05 10:09:35 +0530

    Added login API

--- profiles_api/urls.py
+++ profiles_api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path ,include
 from profiles_api import views
+from rest_framework.routers import DefaultRouter
+
+router = DefaultRouter()
+router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
+router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
 
     path('hello-view/',views.HelloApiView.as_view()),
+    path('login/', views.UserLoginApiView.as_view()),
+    path('',include(router.urls))
+
 
 ]
--- profiles_api/views.py
+++ profiles_api/views.py
@@ -2,7 +2,13 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from profiles_api import serializers
-
+from rest_framework import viewsets
+from profiles_api import models
+from rest_framework.authentication import TokenAuthentication
+from profiles_api import permissions
+from rest_framework import filters
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 
 class HelloApiView(APIView):
     """Test API View"""
@@ -32,6 +38,8 @@ class HelloApiView(APIView):
                     serializer.errors,
                     status=status.HTTP_400_BAD_REQUEST
             )
+
+
     def put(self, request, pk=None):
         return Response({'method':'PUT'})
 
@@ -40,4 +48,59 @@ class HelloApiView(APIView):
 
     def delete(self, request, pk=None):
         return Response({'method':'DELTETE'})
-        
+
+
+
+class HelloViewSet(viewsets.ViewSet):
+
+    serializer_class = serializers.HelloSerializer
+
+    def list(self, request):
+
+        a_viewset = [
+
+            'Uses actions (list, create, retrive ,update ,partial_update)',
+            'Automatically maps to URLs using Routers',
+            'Provides more functionality with less code'
+        ]
+
+        return Response({'message':'Hello!','a_viewset':a_viewset})
+
+
+    def create(self, request):
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            name = serializer.validated_data.get('name')
+            message = f'Hello {name}!'
+            return Response({'message':message})
+        else:
+            return Response(
+                serializer.errors,
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
+    def retrive(self, request, pk=None):
+        return Response({'http_method':'GET'})
+
+    def update(self, request, pk=None):
+        return Response({'http_method':'PUT'})
+
+    def partial_update(self, request, pk=None):
+        return Response({'http_method':'PATCH'})
+
+    def destroy(self, request, pk=None):
+        return Response({'http_method':'DELTETE'})
+
+
+class UserProfileViewSet(viewsets.ModelViewSet):
+
+    serializer_class = serializers.UserProfileSerializer
+    queryset = models.UserProfile.objects.all()
+    authentication_classes = (TokenAuthentication, )
+    permission_classes = (permissions.UpdateOwnProfile, )
+    filter_backends = (filters.SearchFilter, )
+    search_fields = ('name', 'email')
+
+
+class UserLoginApiView(ObtainAuthToken):
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
